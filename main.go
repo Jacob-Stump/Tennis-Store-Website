7452495c65e34a4f2d56945e26a85431c721799d
Jacob-Stump <jakedstump@gmail.com>
2023-08-25 13:03:57 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+func getTableName(w http.ResponseWriter, r *http.Request) {
+	var tableName string
+
+	cfg := mysql.Config{
+		User:   "root",
+		Passwd: "Frodobaggins123",
+		Net:    "tcp",
+		Addr:   "localhost:3306",
+		DBName: "golang",
+	}
+
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	defer db.Close()
+	err = db.QueryRow("SELECT table_name FROM customer").Scan(&tableName)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(w, tableName)
+
+	db.Close()
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage endpoint hit")
+}
+
+func handleRequest() {
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/getTableName", getTableName)
+	log.Fatal(http.ListenAndServe(":5500", myRouter))
+}
+
+func main() {
+	//db, err := sql.Open("mysql", "root:Frodobaggins123@tcp(localhost:3306)/golang")
+
+	handleRequest()
+
+}
