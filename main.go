aabfa6debc38b1793f20e312ea9e756e5a7d188a
Jacob-Stump <jakedstump@gmail.com>
2023-09-01 11:39:21 -0500

    Server Side Functionality, Shop Page Automation,
    ** main.go (backend)
    
    - created struct for product table in database and utilized encoding/json package for mapping Go variables retrieved from database.
    
    - created function to get the products and associated attributes from each column in the mySQL database, and created an array of product objects.
    
    - converted array of product objects to  JSON via the encoding/json package and set an http header.
    
    - changed path string name to /product to better reflect api endpoint usage.
    
    - CORS (Cross-Origin-Resource-Sharing) required headers/methods/and origins to be allowed in order for Fetch('http://localhost:8080/product') in shopPage.JS to fetch the endpoint JSON. Dev tools was able to debug this, I was not. Used "github.com/gorilla/handlers" in tandem with net/http package to allow  all origins(*), all methods(GET, POST, PUT, DELETE, OPTIONS), and all headers(Content-Type). Using mux, allowed myRouter to use the corsMiddleWare handler.
    
    ** shopPage.JS (frontend)
    
    - deprecated the static method of populating an array of product objects, instead implementing a dynamic way of populating products on the shop page via usage of the endpoint JSON on the server.
    
    - removed card div from html page in order to implement a div template within the generateCard method, which generates a card for every instance of a product object within the array converted from the fetched JSON string. JSON string is then converted to an array which is then used in the generateCard function.
    
    - generateCard() utilizes innerHTML in order to insert a templated div. Maps the JSON object types to the corresponding variables within the template, denoted by &{var}. Note that without the function on line 6, you would need to do ${x.var}.
    
    - added a DOMContentLoaded event listener to fetch and load the products on every instance of the shop.html page being loaded.

--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	_ "io/ioutil"
-	"log"
-	"net/http"
+	"database/sql"  //interface for querying/interacting with database
+	"encoding/json" //for formatting/encoding/decoding json strings
+	"log"           //for logging problems/issues
+	"net/http"      //used for http requests/ http.ResponseWriter, *http.Request
 
-	"github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
+	"github.com/go-sql-driver/mysql" //driver for mySQL connection
+	"github.com/gorilla/handlers"    //works with net/http package to validate content types, allow access via CORS, compressing http responses
+	"github.com/gorilla/mux"         //used to make handling multiple path variables/endpoint connections and related methods
 )
 
-func getTableName(w http.ResponseWriter, r *http.Request) {
-	var tableName string
+type Product struct { //creating JSON object structure for DB retrieval
+	ID    int     `json:"product_id"`
+	Name  string  `json:"product_name"`
+	Desc  string  `json:"descr"`
+	Price float32 `json:"price"`
+	Img   string  `json:"imgurl"`
+}
 
+type Customer struct { //need to restructure customer table/ break address into a seperate table
+	ID        int    `json:"customer_id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Phone     int    `json:"phone"`
+}
+
+func getProducts(w http.ResponseWriter, r *http.Request) { //func to take in product info from DB and JSONify
 	cfg := mysql.Config{
 		User:   "root",
 		Passwd: "Frodobaggins123",
@@ -25,37 +39,50 @@ func getTableName(w http.ResponseWriter, r *http.Request) {
 	db, err := sql.Open("mysql", cfg.FormatDSN())
 
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		panic(err)
 	}
 
 	defer db.Close()
-	err = db.QueryRow("SELECT table_name FROM customer").Scan(&tableName)
 
+	rows, err := db.Query("SELECT product_id, product_name, descr, price, imgurl FROM products")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	defer rows.Close()
 
-	fmt.Fprint(w, tableName)
-
-	db.Close()
+	var products []Product
+	for rows.Next() { //for every row in products table..
+		var p Product
+		err := rows.Scan(&p.ID, &p.Name, &p.Desc, &p.Price, &p.Img)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		products = append(products, p)
+	}
+	//returning the product information as JSON
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
 }
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Homepage endpoint hit")
-}
-
-func handleRequest() {
+func handleRequest(corsMiddleware func(http.Handler) http.Handler) { //utilizing mux for handling multiple requests
 
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/getTableName", getTableName)
-	log.Fatal(http.ListenAndServe(":5500", myRouter))
+	myRouter.Use(corsMiddleware)
+	myRouter.HandleFunc("/product", getProducts)
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func main() {
 	//db, err := sql.Open("mysql", "root:Frodobaggins123@tcp(localhost:3306)/golang")
 
-	handleRequest()
+	corsMiddleware := handlers.CORS(
+		handlers.AllowedOrigins([]string{"*"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+		handlers.AllowedHeaders([]string{"Content-Type"}),
+	)
+
+	handleRequest(corsMiddleware)
 
 }
